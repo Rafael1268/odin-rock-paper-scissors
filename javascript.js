e44cd93a2bcc39866b5e7ccf2699a3d85cf75090
RafaelB <rafaelboschmamail@gmail.com>
2022-05-12 17:46:54 +0200

    Made the UI work for the game

--- javascript.js
+++ javascript.js
@@ -2,6 +2,13 @@ let playerScore = 0;
 let computerScore = 0;
 let tiedGames = 0;
 
+// Updates the score board
+function updateScore() {
+    const score = document.querySelector('#scoreBoard');
+    score.textContent = `${playerScore} - ${computerScore}`;
+    return;
+}
+
 // Gets a random choice for the computer
 function computerPlay() {
     randNum = Math.floor(Math.random() * 3);
@@ -19,6 +26,7 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
         tiedGames++
+        updateScore();
         return "tied";
     } if (
         (playerSelection === "rock" && computerSelection === "paper") ||
@@ -26,6 +34,7 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === "scissors" && computerSelection === "rock")
     ) {
         computerScore++
+        updateScore();
         return "lost";
     } if (
         (playerSelection === "rock" && computerSelection === "scissors") ||
@@ -33,37 +42,54 @@ function playRound(playerSelection, computerSelection) {
         (playerSelection === "scissors" && computerSelection === "paper")
     ) {
         playerScore++
+        updateScore();
         return "won";
     } else {
         return "invalid";
     }
 }
 
-// Asks the player for their choice, then checks who won. Repeated 5 times
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerChoice = prompt().toLowerCase();
-        if (!["rock", "paper", "scissors"].includes(playerChoice)) {
-            console.log("Please make sure you typed your choice correctly.")
-        }
-        let playGame = playRound(playerChoice, computerPlay());
-        if (playGame === "invalid") {
-            return;
-        }
-        console.log(`You ${playGame}. You've won ${playerScore} out of ${i + 1} rounds.`);
+// Starts a round when the player clicks a button
+function handleClick(playerChoice) {
+    const computerChoice = computerPlay();
+    const gameResult = playRound(playerChoice, computerChoice);
+    showResult(gameResult, computerChoice);
+    if (playerScore >= 5 || computerScore >= 5) {
+        return endGame();
+    }
+    return;
+}
+
+// Updates the text to show the result of the round
+function showResult(gameResult, computerChoice) {
+    const resultTxt = document.querySelector("#resultTxt");
+    switch (gameResult) {
+        case "won":
+            return resultTxt.textContent = `Computer chose ${computerChoice}, you won!`;
+        case "lost":
+            return resultTxt.textContent = `Computer chose ${computerChoice}, you lost!`;
+        case "tied":
+            return resultTxt.textContent = `Computer chose ${computerChoice}, you tied!`;
+        default:
+            return resultTxt.textContent = "Click one of the buttons to start the game. First to 5 wins!";
     }
 }
 
-// Tells the player the final score
-function endResult() {
-    if (playerScore > computerScore) {
-        console.log(`You won the game! In the end you won ${playerScore} rounds and tied in ${tiedGames} rounds. Reload the page to play again.`)
-    } else if (playerScore < computerScore) {
-        console.log(`You lost the game! In the end you won ${playerScore} rounds and tied in ${tiedGames} rounds. Reload the page to play again.`)
-    } else if (playerScore === computerScore) {
-        console.log(`You tied! In the end you both won ${playerScore} rounds and tied in ${tiedGames} rounds. Reload the page to play again.`)
-    }
+// Ends the game and shows the play again button
+function endGame() {
+    const rpsButtons = document.querySelector(".buttons");
+    rpsButtons.style.display = "none";
+    playAgainBtn.style.display = "flex";
 }
 
-game();
-endResult();
+const rockBtn = document.querySelector("#rockBtn");
+const paperBtn = document.querySelector("#paperBtn");
+const scissorsBtn = document.querySelector("#scissorsBtn");
+const playAgainBtn = document.querySelector("#playAgainBtn");
+
+playAgainBtn.style.display = "none";
+rockBtn.addEventListener('click', () => handleClick("rock"));
+paperBtn.addEventListener('click', () => handleClick("paper"));
+scissorsBtn.addEventListener('click', () => handleClick("scissors"));
+playAgainBtn.addEventListener('click', () => location.reload());
+
